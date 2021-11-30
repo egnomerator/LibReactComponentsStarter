2383e5486ae1842744e48adf3d20eb0831a7b5b4
egnomerator <egnomerator@users.noreply.github.com>
2021-11-30 00:40:41 -0600

    added example Jest snapshot tests (snapshots should be in source control)

---
+++ LibReactComponentsStarter/wwwroot/app/src/components/devsEditor/DevsEditor/index.test.tsx
@@ -0,0 +1,11 @@
+﻿import * as React from "react";
+import * as ReactTestRenderer from "react-test-renderer";
+import { DevsEditor } from ".";
+
+describe("DevsEditor", () => {
+    it("should render (placeholder test)", () => {
+        //const devsEditor = ReactTestRenderer.create(<DevsEditor/>);
+
+        //expect(devsEditor).toMatchSnapshot();
+    });
+});
---
+++ LibReactComponentsStarter/wwwroot/app/src/components/devsEditor/EditableTable/index.test.tsx
@@ -0,0 +1,54 @@
+﻿import * as React from "react";
+import * as ReactTestRenderer from "react-test-renderer";
+import { EditableTable } from ".";
+import { Dev } from "../models/Dev";
+
+describe("EditableTable", () => {
+    it("should render with zero devs", () => {
+        const mockView = () => { };
+        const mockEdit = () => { };
+        const mockDelete = () => { };
+        const devs = [];
+
+        const editableTable = ReactTestRenderer.create(<EditableTable devs={devs} view={mockView} edit={mockEdit} delete={mockDelete} />);
+
+        expect(editableTable).toMatchSnapshot();
+    });
+    it("should render with one dev", () => {
+        const mockView = () => { };
+        const mockEdit = () => { };
+        const mockDelete = () => { };
+        const devs: Dev[] = [{ id: 1, firstName: "first", lastName: "last" }];
+
+        const editableTable = ReactTestRenderer.create(<EditableTable devs={devs} view={mockView} edit={mockEdit} delete={mockDelete} />);
+
+        expect(editableTable).toMatchSnapshot();
+    });
+    it("should render with two devs", () => {
+        const mockView = () => { };
+        const mockEdit = () => { };
+        const mockDelete = () => { };
+        const devs: Dev[] = [
+            { id: 1, firstName: "first", lastName: "last" },
+            { id: 2, firstName: "first", lastName: "last" }
+        ];
+
+        const editableTable = ReactTestRenderer.create(<EditableTable devs={devs} view={mockView} edit={mockEdit} delete={mockDelete} />);
+
+        expect(editableTable).toMatchSnapshot();
+    });
+    it("should render with three devs", () => {
+        const mockView = () => { };
+        const mockEdit = () => { };
+        const mockDelete = () => { };
+        const devs: Dev[] = [
+            { id: 1, firstName: "first", lastName: "last" },
+            { id: 2, firstName: "first", lastName: "last" },
+            { id: 3, firstName: "first", lastName: "last" }
+        ];
+
+        const editableTable = ReactTestRenderer.create(<EditableTable devs={devs} view={mockView} edit={mockEdit} delete={mockDelete} />);
+
+        expect(editableTable).toMatchSnapshot();
+    });
+});
